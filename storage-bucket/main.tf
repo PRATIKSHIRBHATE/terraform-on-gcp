9ab12853edfe712f8dd74e623c44dc96eafe269c
Pratik Shirbhate <pratikshirbhate@gmail.com>
2023-12-29 16:33:01 +0530

    seperated provider file

--- storage-bucket/main.tf
+++ storage-bucket/main.tf
@@ -1,10 +1,3 @@
-provider "google" {
-  project     = var.project
-  credentials = file(var.credentials_file)
-  region      = var.region
-  zone        = var.zone
-}
-
 resource "google_storage_bucket" "terraform_bucket" {
   name     = var.bucket_name
   location = var.region  # Replace with your desired location
