5fbe928a3d461c240cb12d831d5cd5b3b74a588b
Pratik Shirbhate <pratikshirbhate@gmail.com>
2024-02-08 13:55:07 +0530

    added a variable validation condition

--- compute-engine/compute-engine-variables/variables.tf
+++ compute-engine/compute-engine-variables/variables.tf
@@ -14,8 +14,21 @@ variable "os_image" {
 	default = "debian-cloud/debian-11"
 }
 
+variable "environment" {
+  type        = string
+  description = "enviornment name"
+  validation {
+    condition = can(regex("^(production|test|stage|dev)$", var.environment))
+    error_message = "Invalid Enviornment. Must either production|test!stage|dev"
+  }
+  }
+
 variable "instance_type" {
   type        = string
   description = "Disk type of the Google Compute instance"
   default     = "f1-micro"
+  validation {
+    condition = can(regex("^(f1-micro|f1-medium)$", var.instance_type))
+    error_message = "Invalid Instance type. Must be f1-micro"
+  }
   }
