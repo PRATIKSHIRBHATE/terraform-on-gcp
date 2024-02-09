522b47141da42cb5d76f257aa27a83bdcffd4d3f
Pratik Shirbhate <pratikshirbhate@gmail.com>
2024-02-09 09:25:59 +0530

    added a code to pull module from terraform-google-moduless

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
 # Note: We cannot pass the values to the variables for modules at run time 
+# Modules Registry: https://registry.terraform.io/search/modules?namespace=terraform-google-modules
+
 terraform {
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = "5.10.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.10.0"
     }
+  }
 }
 
 provider "google" {
-    project     = "terrafom-on-gcp"
-    credentials = "C:/Users/Pratik.Shirbhate/Desktop/important_documents/learning_projects/terraform-on-gcp/secrets/credentials-terraform-gcp.json"
-    region      = "us-central1"
-    zone        = "us-central1-a"
+  project     = "terrafom-on-gcp"
+  credentials = "C:/Users/Pratik.Shirbhate/Desktop/important_documents/learning_projects/terraform-on-gcp/secrets/credentials-terraform-gcp.json"
+  region      = "us-central1"
+  zone        = "us-central1-a"
 }
-
+/*
 module "compute-instance" {
         source            = "github.com/PRATIKSHIRBHATE/terraform-on-gcp/modules/compute-engine"
         # source            = "./modules/compute-engine"
@@ -34,4 +36,12 @@ module "google-storage-bucket" {
         project           = "terrafom-on-gcp"
         credentials_file  = "C:/Users/Pratik.Shirbhate/Desktop/important_documents/learning_projects/terraform-on-gcp/secrets/credentials-terraform-gcp.json"
         bucket_name       = "terraform-temp-bucket"
+}
+
+*/
+module "google-storage-bucket" {
+  source     = "terraform-google-modules/cloud-storage/google"
+  version    = "~> 5.0"
+  project_id = "terrafom-on-gcp"
+  names      = ["terraform-temp-bucket"]
 }
