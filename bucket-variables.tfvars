611061fc2a46b3797979ba453e40967e90fee9a9
Pratik Shirbhate <pratikshirbhate@gmail.com>
2024-02-09 09:45:37 +0530

    modularized the code, seperated provider and variables

---
+++ bucket-variables.tfvars
@@ -0,0 +1,1 @@
+credentials_file = "C:\\Users\\Pratik.Shirbhate\\Desktop\\important_documents\\learning_projects\\terraform-on-gcp\\secrets\\credentials-terraform-gcp.json"
--- main.tf
+++ main.tf
@@ -1,21 +1,6 @@
 # Note: We cannot pass the values to the variables for modules at run time 
 # Modules Registry: https://registry.terraform.io/search/modules?namespace=terraform-google-modules
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.10.0"
-    }
-  }
-}
-
-provider "google" {
-  project     = "terrafom-on-gcp"
-  credentials = "C:/Users/Pratik.Shirbhate/Desktop/important_documents/learning_projects/terraform-on-gcp/secrets/credentials-terraform-gcp.json"
-  region      = "us-central1"
-  zone        = "us-central1-a"
-}
 /*
 module "compute-instance" {
         source            = "github.com/PRATIKSHIRBHATE/terraform-on-gcp/modules/compute-engine"
@@ -43,5 +28,5 @@ module "google-storage-bucket" {
   source     = "terraform-google-modules/cloud-storage/google"
   version    = "~> 5.0"
   project_id = "terrafom-on-gcp"
-  names      = ["terraform-temp-bucket"]
+  names      = ["terraform-temp-bucket-2"]
 }
