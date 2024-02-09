5ffba17ae53182bd15f97d0e7abfc8d9642825a6
Pratik Shirbhate <pratikshirbhate@gmail.com>
2024-02-09 10:49:04 +0530

    added a code to provier read permission to all users for existing storage bucket

--- iam/main.tf
+++ iam/main.tf
@@ -4,9 +4,15 @@ provider "google" {
 	region = var.region
 	zone = var.zone
 }
-
+/*
 resource "google_project_iam_member" "user_iam_member" {
 	project = var.project
 	role    = "roles/iam.securityAdmin"
 	member  = "user:${var.user_email}"
+}
+*/
+resource "google_storage_bucket_iam_binding" "gcs_bucket_binding" {
+	bucket = "terraform-temp-bucket-2"
+	role = "roles/storage.objectViewer"
+	members = ["allUsers"]
 }
